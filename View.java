5230616bef43a405329b4f61f753b31ef9fb8953
FilipHjermind <filiphjermind@gmail.com>
2011-03-11 15:24:23 +0100

    This copy is not working

--- View.java
+++ View.java
@@ -1,7 +1,11 @@
+import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 public class View extends JFrame{
@@ -26,7 +30,22 @@ public class View extends JFrame{
 	// Filips
 	private void createContent() {
 		Container outer = this.getContentPane();
-		
-		// add components
+		outer.setLayout(new GridLayout(1,2));
+		outer.add(canvas);
+		JPanel menuPanel = new JPanel();
+		menuPanel.setLayout(new GridLayout(2,1));
+		outer.add(menuPanel);
+		JPanel navigationPanel = new JPanel();
+		menuPanel.add(navigationPanel);
+		navigationPanel.setLayout(new BorderLayout());
+		navigationPanel.add(new JButton("^"),BorderLayout.NORTH);
+		navigationPanel.add(new JButton(">"),BorderLayout.EAST);
+		navigationPanel.add(new JButton("v"),BorderLayout.SOUTH);
+		navigationPanel.add(new JButton("<"),BorderLayout.WEST);
+		JPanel centerPanel = new JPanel();
+		centerPanel.setLayout(new GridLayout(2,1));
+		centerPanel.add(new JButton("+"));
+		centerPanel.add(new JButton("-"));
+		navigationPanel.add(centerPanel,BorderLayout.CENTER);
 	}
 }
