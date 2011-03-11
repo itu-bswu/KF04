08763e539b6a78cf54d18bba12fcf18f7c35ef43
Jakob Melnyk <jakobmelnyk@hotmail.com>
2011-03-11 16:01:04 +0100

    Control first edition done

--- Control.java
+++ Control.java
@@ -1,8 +1,11 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Point2D;
 
 public class Control {
 	
+	private static final double MOVE_LENGTH = 0.30;
+	private static final double ZOOM_LENGTH = 0.15;
 	private static final String NAME = "Map";
 	private View v;
 	private Map m;
@@ -19,27 +22,32 @@ public class Control {
 	private void addListeners(){
 		v.addUpListener(new ActionListener(){
 						public void actionPerformed(ActionEvent arg0){
+							m.move(Direction.NORTH, MOVE_LENGTH);
 							v.repaint(m.getLines());
 						}});
 		v.addDownListener(new ActionListener(){
 						public void actionPerformed(ActionEvent arg0){
-							Line[] a = m.getLines();
+							m.move(Direction.SOUTH, MOVE_LENGTH);
 							v.repaint(m.getLines());
 						}});
 		v.addLeftListener(new ActionListener(){
 						public void actionPerformed(ActionEvent arg0){
+							m.move(Direction.WEST, MOVE_LENGTH);
 							v.repaint(m.getLines());
 						}});
 		v.addRightListener(new ActionListener(){
 						public void actionPerformed(ActionEvent arg0){
+							m.move(Direction.EAST, MOVE_LENGTH);
 							v.repaint(m.getLines());
 						}});
 		v.addInListener(new ActionListener(){
 						public void actionPerformed(ActionEvent arg0){
+							m.zoom(new Point2D.Double(ZOOM_LENGTH, ZOOM_LENGTH), new Point2D.Double(1-ZOOM_LENGTH, 1-ZOOM_LENGTH));
 							v.repaint(m.getLines());
 						}});
 		v.addOutListener(new ActionListener(){
 						public void actionPerformed(ActionEvent arg0){
+							m.zoom(new Point2D.Double(-ZOOM_LENGTH, -ZOOM_LENGTH), new Point2D.Double(1+ZOOM_LENGTH, 1+ZOOM_LENGTH));
 							v.repaint(m.getLines());
 						}});
 	}
