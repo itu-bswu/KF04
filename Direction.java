1837a790bca167575876ed0ce00bbf17832f224b
Niklas Hansen <nikl@itu.dk>
2011-03-14 22:42:50 +0100

    Prettyfied and slightly refactored Krak code.

--- Direction.java
+++ Direction.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /**
- * An enum class to get a direction. A random direction can be obtained by calling the getRandomDirection() method.
+ * An enum class to get a direction. A random direction can be obtained by
+ * calling the getRandomDirection() method.
+ * 
  * @author Emil
  * 
  */
@@ -10,11 +12,12 @@ public enum Direction {
 	WEST, EAST, NORTH, SOUTH;
 
 	/**
-	 * Shuffle an ArrayList containing the directions and returns the first value.
+	 * Shuffle an ArrayList containing the directions and returns the first
+	 * value.
+	 * 
 	 * @return randomDirection A random value of the four directions.
 	 */
-	public static Direction getRandomDirection()
-	{
+	public static Direction getRandomDirection() {
 		ArrayList<Direction> directions = new ArrayList<Direction>();
 		for (Direction d : Direction.values())
 			directions.add(d);
@@ -26,17 +29,21 @@ public enum Direction {
 
 	/**
 	 * Get the opposite direction of the one given.
+	 * 
 	 * @param direction the given direction
 	 * @return Direction the opposite direction of the given
 	 */
 	public static Direction getOpposite(Direction direction) {
-		switch(direction) {
-		case NORTH: return SOUTH;
-		case EAST: return WEST;
-		case WEST: return EAST;
-		case SOUTH: return NORTH;
+		switch (direction) {
+		case NORTH:
+			return SOUTH;
+		case EAST:
+			return WEST;
+		case WEST:
+			return EAST;
+		case SOUTH:
+			return NORTH;
 		}
 		return null;
 	}
 }
-
--- NodeData.java
+++ NodeData.java
@@ -9,14 +9,15 @@ public class NodeData {
 	final int KDV_ID;
 	final double X_COORD;
 	final double Y_COORD;
-	
+
 	/**
-	 * Parses node data from line, throws an IOException
-	 * if something unexpected is read
+	 * Parses node data from line, throws an IOException if something unexpected
+	 * is read
+	 * 
 	 * @param line The source line from which the NodeData fields are parsed
 	 * @throws IOException
 	 */
-	public NodeData(String line) throws IOException{
+	public NodeData(String line) throws IOException {
 		DataLine dl = new DataLine(line);
 		ARC = dl.getPositiveInt();
 		KDV = dl.getPositiveInt();
@@ -24,12 +25,13 @@ public class NodeData {
 		X_COORD = dl.getPositiveDouble();
 		Y_COORD = dl.getPositiveDouble();
 	}
-	
+
 	/**
-	 * Returns a string representing the node data in the same format as used in the
-	 * kdv_node_unload.txt file.
+	 * Returns a string representing the node data in the same format as used in
+	 * the kdv_node_unload.txt file.
 	 */
-	public String toString(){
-		return "" + ARC + "," + KDV + "," + KDV_ID + "," + X_COORD + "," + Y_COORD;
+	public String toString() {
+		return "" + ARC + "," + KDV + "," + KDV_ID + "," + X_COORD + ","
+				+ Y_COORD;
 	}
 }
