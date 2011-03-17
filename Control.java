149a3a37ed7ccff306da7e1d6a8a1dde714efb3f
Jakob Melnyk <jakobmelnyk@hotmail.com>
2011-03-17 10:38:11 +0100

    changed objects given to map.zoom from points to a rectangle

--- Control.java
+++ Control.java
@@ -16,7 +16,7 @@ public class Control {
 	
 	private static final double MOVE_LENGTH = 0.30;
 	private static final double ZOOM_LENGTH = 0.15;
-	private static final String NAME = "Map";
+	private static final String NAME = "Map"; //Name of the window containing the map.
 	private String nodeFile = "C:/Users/Jakob Melnyk/workspace/KF04/src/data/Charlottenlund/kdv_node_unload.txt";
 	private String edgeFile = "C:/Users/Jakob Melnyk/workspace/KF04/src/data/Charlottenlund/kdv_unload.txt";
 	private View v;
@@ -30,8 +30,7 @@ public class Control {
 		try {
 			g = KrakLoader.graphFromFiles(nodeFile, edgeFile);
 		} catch (IOException e) {
-			//TODO Catch exception
-			e.printStackTrace();
+			System.out.println("A problem occured when trying to read input.");
 		}
 		m = new Map(g);
 		v = new View(NAME, m.getLines());
@@ -39,44 +38,45 @@ public class Control {
 	}
 	
 	private void addListeners(){
+		//Listener for "move-up" button.
 		v.addUpListener(new ActionListener(){
 						public void actionPerformed(ActionEvent arg0){
 							m.move(Direction.NORTH, MOVE_LENGTH);
 							v.repaint(m.getLines());
 						}});
+		//Listener for "move-down" button.
 		v.addDownListener(new ActionListener(){
 						public void actionPerformed(ActionEvent arg0){
 							m.move(Direction.SOUTH, MOVE_LENGTH);
 							v.repaint(m.getLines());
 						}});
+		//Listener for "move-left" button.
 		v.addLeftListener(new ActionListener(){
 						public void actionPerformed(ActionEvent arg0){
 							m.move(Direction.WEST, MOVE_LENGTH);
 							v.repaint(m.getLines());
 						}});
+		//Listener for "move-right" button.
 		v.addRightListener(new ActionListener(){
 						public void actionPerformed(ActionEvent arg0){
 							m.move(Direction.EAST, MOVE_LENGTH);
 							v.repaint(m.getLines());
 						}});
+		//Listener for "zoom-in" button.
 		v.addInListener(new ActionListener(){
 						public void actionPerformed(ActionEvent arg0){
-							m.
-							m.zoom(ZOOM_LENGTH, ZOOM_LENGTH1,-ZOOM_LENGTH, 1-ZOOM_LENGTH);
+							Rectangle2D.Double i = m.getBounds();
+							//Constructs a new rectangle using the maps bounds and the ZOOM_LENGTH variable.
+							m.zoom(new Rectangle2D.Double(i.x + i.width * ZOOM_LENGTH, i.y + i.height * ZOOM_LENGTH, i.width - i.width * ZOOM_LENGTH, i.height - i.height * ZOOM_LENGTH));
 							v.repaint(m.getLines());
 						}});
+		//Listener for "zoom-out" button.
 		v.addOutListener(new ActionListener(){
 						public void actionPerformed(ActionEvent arg0){
-							m.zoom(new Point2D.Double(-ZOOM_LENGTH, -ZOOM_LENGTH), new Point2D.Double(1+ZOOM_LENGTH, 1+ZOOM_LENGTH));
+							Rectangle2D.Double i = m.getBounds();
+							//Constructs a new rectangle using the maps bounds and the ZOOM_LENGTH variable.
+							m.zoom(new Rectangle2D.Double(i.x - i.width * ZOOM_LENGTH, i.y - i.height * ZOOM_LENGTH, i.width + i.width * ZOOM_LENGTH, i.height + i.height * ZOOM_LENGTH));
 							v.repaint(m.getLines());
 						}});
-	}
-	
-	private Rectangle2D.Double makeRect(double a, double b, double c, double d){
-		double e = Math.abs(Math.abs(a) - Math.abs(b));
-		double f = Math.abs(Math.abs(c) - Math.abs(d));
-		Rectangle2D.Double r = new Rectangle2D.Double(a, b, c, d);
-		return r;
-		
-	}
+		}
 }
