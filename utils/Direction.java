0a5a1822abf927b57385c7ee0a37e5964f7c0687
Emil_LAPTOP <emilovicmail@gmail.com>
2011-04-06 14:58:17 +0200

    added Direction again

---
+++ utils/Direction.java
@@ -0,0 +1,11 @@
+package utils;
+
+/**
+ * An enum class to get a direction. A random direction can be obtained by calling the getRandomDirection() method.
+ * @author Emil
+ * 
+ */
+public enum Direction {
+	WEST, EAST, NORTH, SOUTH;
+}
+
