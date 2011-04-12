d7f21e8f3bd04004e02276a4e98f32d38fa9d391
Emil_LAPTOP <emilovicmail@gmail.com>
2011-04-12 12:39:31 +0200

    methods in model for finding closest edge, node and roadname from a point

--- core/Control.java
+++ core/Control.java
@@ -116,7 +116,7 @@ public class Control {
 			public void mouseMoved(MouseEvent e){
 				// set label to closest road
 				Point2D.Double p = PointMethods.pixelToUTM(e.getPoint(), model, view);
-				String roadName = model.getClosestRoad(p);
+				String roadName = model.getClosestRoadname(p);
 				view.setLabel(roadName);
 			}
 		});
