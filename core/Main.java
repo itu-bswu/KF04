04006fc2e33898abaf7346b403e154322c62b09a
Emil_LAPTOP <emilovicmail@gmail.com>
2011-04-12 11:05:58 +0200

    removed exception catch again and added numbering of pins

--- core/Main.java
+++ core/Main.java
@@ -1,17 +1,10 @@
 package core;
-import javax.swing.JOptionPane;
 
 import core.Control;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Control c = null;
-		try{
-		c = new Control();
-		}catch(Exception e){
-			JOptionPane.showMessageDialog(null, e.toString());
-			c = new Control();
-		}
+		Control c = new Control();
 	}
 }
