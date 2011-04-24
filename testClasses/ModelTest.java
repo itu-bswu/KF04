8fe613132f7c6cdd64d8898ed542e9e953a37c33
Jens Møllerhøj <mollerhoj3@hotmail.com>
2011-04-24 22:35:47 +0200

    modeltest modified

--- testClasses/ModelTest.java
+++ testClasses/ModelTest.java
@@ -1,36 +1,53 @@
 package testClasses;
 import static junit.framework.Assert.*;
 
+import graphlib.Edge;
+import graphlib.Graph;
+import graphlib.Node;
 import gui.Line;
 
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 import java.util.HashSet;
+
+import junit.framework.TestCase;
 
 import org.junit.*;
 
 import core.Model;
+import dataobjects.KrakNode;
 
 /**
  * Test Class for the model
  * @author mollerhoj3
  *
  */
-public class ModelTest {
+public class ModelTest extends TestCase{
 	private static Model model;
 	
 	//TODO skal der laves en kunstig graf man kan køre tests på??
 	/**
 	 * Before Class
 	 */
-	@BeforeClass public static void BeforeClass() {		
+	@BeforeClass public static void BeforeClass() {
+		
+		System.out.println("Testing model");
+		
 		model =  new Model();
 	}	
 	
 	/**
+	 * Fake graph tests
+	 */
+	@Test public void testFakeGraph() {
+		
+	}
+	
+	/**
 	 * Update Bounds Tests
 	 */
-	@Test public void updateBoundsTestNull() {
+	@Test public void testUpdateBoundsNull() {
 		try {
 		model.updateBounds(null);
 		}
@@ -39,7 +56,7 @@ public class ModelTest {
 		}
 	}
 	
-	@Test public void updateBoundsTestWidth() {
+	@Test public void testUpdateBoundsWidth() {
 		try {
 			model.updateBounds(new Rectangle2D.Double(0,0,-1,0));
 		}catch (IllegalArgumentException e) {
@@ -47,7 +64,7 @@ public class ModelTest {
 		}
 	}
 	
-	@Test public void updateBoundsTestHeight() {
+	@Test public void testUpdateBoundsHeight() {
 		try {
 			model.updateBounds(new Rectangle2D.Double(0,0,0,-1));
 		}catch (IllegalArgumentException e) {
@@ -55,7 +72,7 @@ public class ModelTest {
 		}
 	}
 	
-	@Test public void updateBoundsTestOutOfBounds() {
+	@Test public void testUpdateBoundsOutOfBounds() {
 		try {
 			model.updateBounds(new Rectangle2D.Double(-10,-10,100,70));			
 		}catch (IllegalArgumentException e) {
@@ -66,26 +83,16 @@ public class ModelTest {
 	/**
 	 * Since the graph is empty, we expect an empty set of lines.
 	 */
-	@Test public void getLinesTest() {
+	@Test public void testGetLines() {
 		assertEquals(new HashSet<Line>(), model.getLines());
 	}
 	
-	/**
-	 * Test Relative point
-	 */
-	/*
-	 TODO kan man teste private metoder? Det ville være ret nice.
-	@Test public void relativePointTest() {
-		model.updateBounds(new Rectangle2D.Double(0,0,100,100));
-		assertEquals();
-	}
-	*/
 	
 	/**
 	 * Since the graph is empty, we expect an empty set of lines.
 	 */
 	//TODO vi burde virkelig have en falsk graph
-	@Test public void getClosestRoadTest() {
+	@Test public void testGetClosestRoad() {
 		//assertEquals(" ", model.getClosestRoad(new Point2D.Double(0,0)));
 	}
 	
