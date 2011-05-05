dc4503908e01ed0499427ec165f82dfb1337face
Niklas Hansen <nikl@itu.dk>
2011-05-06 01:11:06 +0200

    Removed time-logging from View.

--- gui/View.java
+++ gui/View.java
@@ -560,7 +560,7 @@ public class View extends JFrame{
 		 */
 		public void drawOffScreen(Collection<Line> lines){
 			if(getWidth() > 0 && getHeight() > 0){
-				Stopwatch timer = new Stopwatch("Drawing");
+				//Stopwatch timer = new Stopwatch("Drawing");
 				img = new BufferedImage(getWidth(),getHeight(),BufferedImage.TYPE_INT_RGB);
 				Graphics2D g = (Graphics2D) img.getGraphics();
 				
@@ -590,7 +590,7 @@ public class View extends JFrame{
 				}
 
 				g.dispose();
-				timer.printTime();
+				//timer.printTime();
 			}
 		}
 
